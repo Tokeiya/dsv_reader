1b003952d717fd2af3cc69f335f47cb8df988770
Tokeiya <net_seed@hotmail.com>
2026-04-26 17:49:20 +0900

    Refactor `MemBuffer` to use `Option<usize>` for `length` and update `Buffer` trait methods

--- dsv_reader/src/mem_buffer.rs
+++ dsv_reader/src/mem_buffer.rs
@@ -1,5 +1,4 @@
 use crate::buffer::Buffer;
-use std::arch::x86_64::_mm_undefined_si128;
 use std::io::Read;
 
 const SIZE: usize = 8_192;
@@ -8,7 +7,7 @@ pub struct MemBuffer<R> {
 	backend: R,
 	storage: [u8; SIZE],
 	index: usize,
-	length: usize,
+	length: Option<usize>,
 }
 
 impl<R> MemBuffer<R> {
@@ -17,54 +16,89 @@ impl<R> MemBuffer<R> {
 			backend,
 			storage: [0; SIZE],
 			index: 0,
-			length: 0,
+			length: None,
 		}
 	}
 }
 
 impl<R: Read> MemBuffer<R> {
 	fn load(&mut self) -> Result<(), std::io::Error> {
-		if self.index == self.length {
-			self.length = self.backend.read(&mut self.storage)?;
+		if self.length.is_none() || self.index == self.length.unwrap() {
+			self.length = Some(self.backend.read(&mut self.storage)?);
 			self.index = 0;
 		} else {
 			let mut idx = 0;
-
-			for i in self.index..self.length {
+			
+			for i in self.index..self.length.unwrap() {
 				self.storage[idx] = self.storage[i];
 				idx += 1;
 			}
-
-			self.length = self.backend.read(&mut self.storage[idx..])? + idx;
+			
+			self.length = Some(self.backend.read(&mut self.storage[idx..])? + idx);
 			self.index = 0;
 		}
-
+		
+		dbg!(format!("idx:{} len{}", self.index, self.length.unwrap()));
+		
 		Ok(())
 	}
 }
 
 impl<R: std::io::Read> Buffer for MemBuffer<R> {
 	type Error = std::io::Error;
-
+	
 	fn read(&mut self) -> Result<Option<u8>, Self::Error> {
-		self.index += 1;
-		if self.index >= self.length {
+		if let None = self.length {
 			self.load()?;
-		};
-
-		if self.length == 0 {
-			Ok(None);
-		} else {
 		}
-
-		todo!()
+		
+		let len = match self.length {
+			None => unreachable!(),
+			Some(l) => l
+		};
+		
+		if len == 0 {
+			return Ok(None);
+		}
+		
+		if self.index == len {
+			self.load()?;
+		}
+		
+		if self.length == Some(0) {
+			Ok(None)
+		} else {
+			let value = self.storage[self.index];
+			self.index += 1;
+			Ok(Some(value))
+		}
 	}
-
-	fn peek(&self) -> Result<Option<u8>, Self::Error> {
-		todo!()
+	
+	fn peek(&mut self) -> Result<Option<u8>, Self::Error> {
+		if self.length == None {
+			self.load()?;
+		}
+		
+		let len = match self.length {
+			None => unreachable!(),
+			Some(len) => len
+		};
+		
+		if len == 0 {
+			return Ok(None);
+		}
+		if len == self.index {
+			self.load()?;
+		}
+		
+		if self.length == Some(0) {
+			Ok(None)
+		} else {
+			Ok(Some(self.storage[self.index]))
+		}
 	}
-
-	fn look_ahead_1(&self) -> Result<Option<u8>, Self::Error> {
+	
+	fn look_ahead_1(&mut self) -> Result<Option<u8>, Self::Error> {
 		todo!()
 	}
 }
@@ -74,23 +108,23 @@ mod tests {
 	use super::*;
 	use mockall::mock;
 	use std::io::Read;
-
+	
 	mock! {
 		Reader{}
 		impl Read for Reader{
 			fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> ;
 		}
 	}
-
+	
 	#[test]
 	fn new() {
 		let fixture = MemBuffer::new(MockReader::new());
-		assert_eq!(fixture.index, usize::MAX);
+		assert_eq!(fixture.index, 0);
 		assert!(fixture.storage.iter().all(|&x| x == 0));
 		assert_eq!(fixture.storage.len(), SIZE);
-		assert_eq!(fixture.length, 0);
+		assert_eq!(fixture.length, None);
 	}
-
+	
 	#[test]
 	fn read() {
 		let mut mock = MockReader::new();
@@ -100,62 +134,64 @@ mod tests {
 			}
 			Ok(256)
 		});
-
+		
 		mock.expect_read().times(1).returning(|_| Ok(0));
-
+		
 		let mut fixture = MemBuffer::new(mock);
-
+		
 		for i in 0..=0xff {
+			dbg!(i);
 			assert_eq!(fixture.read().unwrap().unwrap(), i);
 		}
-
+		
 		assert!(fixture.read().unwrap().is_none());
 		assert!(fixture.read().unwrap().is_none());
 		assert!(fixture.read().unwrap().is_none());
 	}
-
+	
 	#[test]
 	fn peek() {
 		let mut mock = MockReader::new();
-
+		
 		mock.expect_read().times(1).returning(|arr| {
 			for i in 0..=0xff {
 				arr[i as usize] = i;
 			}
 			Ok(256)
 		});
-
+		
 		mock.expect_read().times(1).returning(|_| Ok(0));
-
+		
 		let mut fixture = MemBuffer::new(mock);
+		assert_eq!(fixture.peek().unwrap().unwrap(), 0x00);
 		fixture.index = 0xff;
 		assert_eq!(fixture.peek().unwrap(), Some(0xff));
-
-		fixture.read();
+		
+		_ = fixture.read().unwrap().unwrap();
 		assert_eq!(fixture.peek().unwrap(), None);
 		assert_eq!(fixture.peek().unwrap(), None);
 	}
-
+	
 	#[test]
 	fn look_ahead_1() {
 		let mut mock = MockReader::new();
-
+		
 		mock.expect_read().times(1).returning(|arr| {
 			arr[0] = 0x01;
 			Ok(1)
 		});
-
+		
 		mock.expect_read().times(1).returning(|arr| {
 			arr[0] = 0x02;
 			Ok(1)
 		});
-
+		
 		mock.expect_read().times(1).returning(|_| Ok(0));
-
+		
 		let mut fixture = MemBuffer::new(mock);
 		assert_eq!(fixture.look_ahead_1().unwrap(), Some(0x02));
 		fixture.read().unwrap();
-
+		
 		assert_eq!(fixture.look_ahead_1().unwrap(), None);
 	}
 }
