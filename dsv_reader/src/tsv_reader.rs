2ab52301283fa257b611da2ee00a600684c45406
Tokeiya <net_seed@hotmail.com>
2026-04-27 17:24:16 +0900

    Implement TSV reading logic with support for quotes, tabs, and newlines

--- dsv_reader/src/tsv_reader.rs
+++ dsv_reader/src/tsv_reader.rs
@@ -2,7 +2,6 @@ use super::buffer::Buffer;
 use super::reader::Reader;
 use crate::mem_buffer::MemBuffer;
 use std::io::Read;
-
 const CR: u8 = b'\r';
 const LF: u8 = b'\n';
 const QUOTE: u8 = b'"';
@@ -18,17 +17,48 @@ impl<R: Read> TsvReader<MemBuffer<R>> {
 			buffer: MemBuffer::new(read),
 		}
 	}
+	
+	fn read_unquoted(&mut self) -> Result<Option<(String, bool)>, std::io::Error> {
+		todo!()
+	}
+	
+	fn read_quoted(&mut self) -> Result<Option<(String, bool)>, std::io::Error> {
+		todo!()
+	}
+	
+	fn consume_new_line(&mut self) -> Result<(), std::io::Error> {
+		todo!()
+	}
 }
 
 impl<R: Read> Reader for TsvReader<MemBuffer<R>> {
 	type E = std::io::Error;
 	fn read(&mut self) -> Result<Option<(String, bool)>, Self::E> {
 		let piv = self.buffer.peek()?;
-
+		
 		if piv.is_none() {
 			return Ok(None);
 		}
-
-		todo!()
+		
+		match piv.unwrap() {
+			CR => {
+				self.consume_new_line()?;
+				Ok(Some((String::new(), true)))
+			}
+			LF => {
+				self.consume_new_line()?;
+				Ok(Some((String::new(), true)))
+			}
+			TAB => {
+				_ = self.buffer.read()?;
+				Ok(Some((String::new(), false)))
+			}
+			QUOTE => {
+				self.read_quoted()
+			}
+			_ => {
+				self.read_unquoted()
+			}
+		}
 	}
 }
