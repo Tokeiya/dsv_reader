fc082012619cbbb352b54ffbd6a55b0e02b03f7d
tokeiya3@Home <net_seed@hotmai.com>
2026-05-02 20:08:28 +0900

    Refactor error handling and token processing in `DsvReader`; improve formatting and streamline `read` methods

--- dsv_reader/src/dsv_read.rs
+++ dsv_reader/src/dsv_read.rs
@@ -2,7 +2,6 @@ use super::token_stream::TokenStream;
 use std::string::FromUtf8Error;
 use thiserror::Error as ThisError;
 
-
 #[derive(ThisError)]
 pub enum Error<E: std::error::Error> {
 	#[error("Unexpected end of file")]
@@ -12,23 +11,21 @@ pub enum Error<E: std::error::Error> {
 	#[error("Unexpected delimiter")]
 	UnexpectedDelimiter,
 	#[error("Token stream error: {source}")]
-	TokenStreamError {
-		source: E,
-		remaining: Vec<u8>,
-	},
+	TokenStreamError { source: E, remaining: Vec<u8> },
 	#[error(transparent)]
-	FromUtf8Error(#[from]FromUtf8Error),
-	
+	FromUtf8Error(#[from] FromUtf8Error),
+	#[error("Unexpected new line")]
+	UnexpectedNewLine,
 }
 
 pub trait DsvRead<R: TokenStream, const D: u8> {
 	fn read(&mut self) -> Result<Option<(String, bool)>, Error<R::Error>>;
 	fn fill(&mut self, buffer: &mut Vec<String>) -> Result<Option<usize>, Error<R::Error>> {
 		let mut cnt = 0;
-		
+
 		loop {
 			let piv = self.read()?;
-			
+
 			match piv {
 				None => break,
 				Some((s, b)) => {
@@ -40,7 +37,7 @@ pub trait DsvRead<R: TokenStream, const D: u8> {
 				}
 			}
 		}
-		
+
 		Ok(Some(cnt))
 	}
 }
--- dsv_reader/src/dsv_reader.rs
+++ dsv_reader/src/dsv_reader.rs
@@ -9,12 +9,20 @@ impl<R: TokenStream, const D: u8> DsvReader<R, D> {
 	pub fn new(token_stream: R) -> Self {
 		Self { token_stream }
 	}
-	
+
 	fn read(&mut self, buff: Option<&Vec<u8>>) -> Result<Token, Error<R::Error>> {
-		self.token_stream.advance_token()
-			.map_err(|err| Error::TokenStreamError { source: err, remaining: if let Some(b) = buff { b.clone() } else { Vec::new() } })
+		self.token_stream
+			.advance_token()
+			.map_err(|err| Error::TokenStreamError {
+				source: err,
+				remaining: if let Some(b) = buff {
+					b.clone()
+				} else {
+					Vec::new()
+				},
+			})
 	}
-	
+
 	fn peek(&mut self, buff: Option<&Vec<u8>>) -> Result<&Token, Error<R::Error>> {
 		if self.token_stream.current_token().is_err() {
 			Err(self.read(buff).unwrap_err())
@@ -22,20 +30,20 @@ impl<R: TokenStream, const D: u8> DsvReader<R, D> {
 			Ok(self.token_stream.current_token().unwrap())
 		}
 	}
-	
-	fn read_normal(&mut self) -> Result<Option<(String, bool)>, Error<R::Error>> {
-		let mut buff = Vec::new();
+
+	fn read_normal(
+		&mut self,
+		mut buff: Vec<u8>,
+	) -> Result<Option<(String, bool)>, Error<R::Error>> {
 		let is_eol;
-		
+
 		loop {
-			match self.read(Some(&mut buff))? {
+			match self.read(Some(&buff))? {
 				Token::Delimiter(_) => {
 					is_eol = false;
 					break;
 				}
-				Token::Quoted => {
-					return Err(Error::UnexpectedQuote)
-				}
+				Token::Quoted => return Err(Error::UnexpectedQuote),
 				Token::CR => {
 					is_eol = true;
 					break;
@@ -48,77 +56,71 @@ impl<R: TokenStream, const D: u8> DsvReader<R, D> {
 					is_eol = true;
 					break;
 				}
-				Token::Value(v) => {
-					buff.extend_from_slice(v.as_slice())
-				}
-				Token::EOF => { return Err(Error::UnexpectedEOF) }
+				Token::Value(v) => buff.extend_from_slice(v.as_slice()),
+				Token::EOF => return Err(Error::UnexpectedEOF),
 			}
-		};
-		
-		let str = String::from_utf8(buff).map_err(|err| Error::FromUtf8Error(err))?;
+		}
+
+		let str = String::from_utf8(buff).map_err(Error::FromUtf8Error)?;
 		Ok(Some((str, is_eol)))
 	}
-	
+
 	fn read_quoted(&mut self) -> Result<Option<(String, bool)>, Error<R::Error>> {
 		let mut buff = Vec::new();
-		let is_eol: bool;
-		
 		loop {
 			match self.read(Some(&buff))? {
-				Token::Delimiter(_) => {
-					return Err(Error::UnexpectedDelimiter)
-				}
-				Token::Quoted => {
-					match self.peek(Some(&buff))? {
-						Token::Delimiter(_) => todo!(),
-						Token::Quoted => buff.push(b'"'),
-						Token::CR => todo!(),
-						Token::LF => todo!(),
-						Token::CRLF => todo!(),
-						Token::Value(_) => return Err(Error::UnexpectedQuote),
-						Token::EOF => todo!()
+				Token::Delimiter(_) => return Err(Error::UnexpectedDelimiter),
+				Token::Quoted => match self.peek(Some(&buff))? {
+					Token::Delimiter(_) => {
+						_ = self.read(Some(&buff))?;
+						let str = String::from_utf8(buff).map_err(Error::FromUtf8Error)?;
+						return Ok(Some((str, false)));
 					}
-				}
+					Token::Quoted => buff.push(b'"'),
+					Token::CR => {
+						_ = self.read(Some(&buff))?;
+						let str = String::from_utf8(buff).map_err(Error::FromUtf8Error)?;
+						return Ok(Some((str, true)));
+					}
+					Token::LF => {
+						_ = self.read(Some(&buff))?;
+						let str = String::from_utf8(buff).map_err(Error::FromUtf8Error)?;
+						return Ok(Some((str, true)));
+					}
+					Token::CRLF => {
+						_ = self.read(Some(&buff))?;
+						let str = String::from_utf8(buff).map_err(Error::FromUtf8Error)?;
+						return Ok(Some((str, true)));
+					}
+					Token::Value(_) => return Err(Error::UnexpectedQuote),
+					Token::EOF => {
+						_ = self.read(Some(&buff))?;
+						let str = String::from_utf8(buff).map_err(Error::FromUtf8Error)?;
+						return Ok(Some((str, true)));
+					}
+				},
 				Token::CR => buff.push(b'\r'),
 				Token::LF => buff.push(b'\n'),
 				Token::CRLF => buff.extend_from_slice(b"\r\n"),
 				Token::Value(vec) => buff.extend_from_slice(vec.as_slice()),
-				Token::EOF => return Err(Error::UnexpectedEOF)
+				Token::EOF => return Err(Error::UnexpectedEOF),
 			}
 		}
-		
-		todo!()
 	}
 }
-
 
 impl<R: TokenStream, const D: u8> DsvRead<R, D> for DsvReader<R, D> {
 	fn read(&mut self) -> Result<Option<(String, bool)>, Error<R::Error>> {
 		let piv = self.read(None)?;
-		
+
 		match piv {
-			Token::Delimiter(d) => {
-				return Ok(Some(("".to_string(), false)));
-			}
-			Token::Quoted => {
-				todo!()
-			}
-			Token::CR => {
-				return Ok(Some(("".to_string(), true)));
-			}
-			Token::LF => {
-				return Ok(Some(("".to_string(), true)));
-			}
-			Token::CRLF => {
-				return Ok(Some(("".to_string(), true)));
-			}
-			Token::Value(v) => {
-				todo!()
-			}
-			Token::EOF => {
-				return Ok(None)
-			}
-		};
+			Token::Delimiter(d) => Ok(Some(("".to_string(), false))),
+			Token::Quoted => self.read_quoted(),
+			Token::CR => Ok(Some(("".to_string(), true))),
+			Token::LF => Ok(Some(("".to_string(), true))),
+			Token::CRLF => Ok(Some(("".to_string(), true))),
+			Token::Value(v) => self.read_normal(v),
+			Token::EOF => Ok(None),
+		}
 	}
 }
-
